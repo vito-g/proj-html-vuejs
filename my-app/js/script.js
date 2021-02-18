34f2d4de1c73032a597fad61ecc7529d1a41d3b9
Vito Gentile <gentilevito1076@gmail.com>
2021-02-18 21:45:43 +0100

    SHOP NOW Button: Formattazione Preliminare

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -2,7 +2,8 @@ new Vue({
 
  el: '#root',
  data: {
-   ul: ["Home", "Shop", "Products", "Categories", "News", "Elements"]
+   ul: ["Home", "Shop", "Products", "Categories", "News", "Elements"],
+   buttonShop: "SHOP NOW"
  }
 });
 Vue.config.devtools = true;
