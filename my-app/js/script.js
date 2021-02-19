5ff49426ef8148febb3c2c1efa2c1d61d9f7d472
Vito Gentile <gentilevito1076@gmail.com>
2021-02-19 15:52:48 +0100

    MILESTONE 1 OK: HEADER ( Comprensivo di Jumbotron) + Vue

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -5,8 +5,7 @@ new Vue({
    ul: ["Home", "Shop", "Products", "Categories", "News", "Elements"],
    buttonShop: "SHOP NOW",
    elIcons: ["fab fa-facebook-f", "fab fa-twitter", "fab fa-instagram","fab fa-youtube"],
-   elShopping: ["Shopping Cart", "My Account", "CART"],
-   cart: '#2f3034'
+   elShopping: ["Shopping Cart", "My Account", "CART"]
  }
 });
 Vue.config.devtools = true;
