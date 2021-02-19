dc4277aa95110a539ee9464e25b0c44a5c8f7021
Vito Gentile <gentilevito1076@gmail.com>
2021-02-19 16:15:07 +0100

    BUTTON-Basis: Correzione Font Weight

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -3,7 +3,7 @@ new Vue({
  el: '#root',
  data: {
    ul: ["Home", "Shop", "Products", "Categories", "News", "Elements"],
-   buttonShop: "SHOP NOW",
+   buttonShop: "SHOP NOW!",
    elIcons: ["fab fa-facebook-f", "fab fa-twitter", "fab fa-instagram","fab fa-youtube"],
    elShopping: ["Shopping Cart", "My Account", "CART"]
  }
