c4ffc8da4cf719d158f66005f00b79ba17d5b37c
Vito Gentile <gentilevito1076@gmail.com>
2021-02-19 09:50:27 +0100

    Aggiornamento + Social Icons OK

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -3,7 +3,8 @@ new Vue({
  el: '#root',
  data: {
    ul: ["Home", "Shop", "Products", "Categories", "News", "Elements"],
-   buttonShop: "SHOP NOW"
+   buttonShop: "SHOP NOW",
+   elIcons: ["fab fa-facebook-f", "fab fa-twitter", "fab fa-instagram","fab fa-youtube"]
  }
 });
 Vue.config.devtools = true;
