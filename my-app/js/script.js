30a5b700e169e4f288ceabaaba5862cced31d938
Vito Gentile <gentilevito1076@gmail.com>
2021-02-19 11:09:27 +0100

    HEADER Top Stripe/Right: OK

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -4,7 +4,8 @@ new Vue({
  data: {
    ul: ["Home", "Shop", "Products", "Categories", "News", "Elements"],
    buttonShop: "SHOP NOW",
-   elIcons: ["fab fa-facebook-f", "fab fa-twitter", "fab fa-instagram","fab fa-youtube"]
+   elIcons: ["fab fa-facebook-f", "fab fa-twitter", "fab fa-instagram","fab fa-youtube"],
+   elShopping: ["Shopping Cart", "My Account", "CART"],
  }
 });
 Vue.config.devtools = true;
